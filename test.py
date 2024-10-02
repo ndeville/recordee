2d86b2f510d1e9ce6e231fff52188f3c24b893c3
Nic <github@nicolasdeville.com>
2024-10-02 11:57:38 +0200

    2024-10-02 11:56:44

--- test.py
+++ test.py
@@ -1,126 +1,30 @@
-from datetime import datetime
+
+
+
 import os
-print("----------")
-ts_file = f"{datetime.now().strftime('%y%m%d-%H%M')}"
-ts_db = f"{datetime.now().strftime('%Y-%m-%d %H:%M')}"
-ts_time = f"{datetime.now().strftime('%H:%M:%S')}"
-print(f"{ts_time} starting {os.path.basename(__file__)}")
-import time
-start_time = time.time()
+import mimetypes
 
-from dotenv import load_dotenv
-load_dotenv()
+def is_audio_file(file_path):
+    mime_type, _ = mimetypes.guess_type(file_path)
+    return mime_type and mime_type.startswith('audio')
 
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-print()
-count = 0
-count_row = 0
+def find_audio_files(folder_path):
+    # Expand the tilde to the full home directory path
+    folder_path = os.path.expanduser(folder_path)
 
-print(f"{os.path.basename(__file__)} boilerplate loaded -----------")
-print()
-####################
-# Transcribe One File
+    # os.walk will go through all files and directories, including hidden ones
+    for root, dirs, files in os.walk(folder_path, topdown=True):
+        try:
+            # print(f"Searching in: {root}")
+            for file in files:
+                file_path = os.path.join(root, file)
+                print(f"Checking file: {file_path}")
+                if is_audio_file(file_path):
+                    print(f"Audio file found: {file_path}")
+        except PermissionError as e:
+            print(f"Permission denied for {root}. Error: {e}")
 
-import whisper
-from whisper.utils import get_writer
-import warnings
-# from whisper.utils import get_writer
-import re
-import shutil
-from collections import namedtuple # to return transcript result as namedtuple
-import os, os.path
-from pathlib import Path
-import sys
-import moviepy.editor # to calculate video duration
-
-from openaee_get import ai_transcript_processing
+folder_path = "~/Library/Containers/com.apple.VoiceMemos"
+find_audio_files(folder_path)
 
 
-""" TODO
-
-TEST https://github.com/pyannote to diariaze speakers
-
-"""
-
-
-
-warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
-
-def transcribe_file(file_path,model_size="medium"):
-
-    print(f"\n\n{datetime.now().strftime('%H:%M:%S')} PROCESSING AS SINGLE RECORDING: {file_path}\n\n")
-
-    filepath_parts = Path(file_path).parts
-    uid = filepath_parts[-1]
-    copy_to_path = os.path.abspath(os.path.join(file_path, os.pardir))
-
-    # Run model
-    model = whisper.load_model(model_size)
-
-    # Transcribe the audio file
-    result = model.transcribe(file_path)
-
-    # Extract the transcription from the result
-    transcript = result['text']
-
-    # print(f"\n\nTranscript:\n{transcript}\n\n")
-
-    # output = transcript
-
-    # output = f"\n{file}\ntranscribed: {ts_db} | {transcribe_language}\n---\n{transcript}\n\n"
-
-    ### txt
-    output_file = f"{copy_to_path}/{uid}.txt"
-    with open(output_file, 'w') as f:
-        print(transcript, file=f)
-    print(f"\n{output_file} created.")
-
-    # Enriched Markdown
-
-    enriched_transcript = ai_transcript_processing(transcript)
-
-    final_transcript = f"## RAW TRANSCRIPT\n{file_path}\n\n{transcript}\n\n{enriched_transcript}"
-
-    output_file = f"/Users/nic/Dropbox/Notes/kaltura/transcripts/{uid}.md"
-    with open(output_file, 'w') as f:
-        print(final_transcript, file=f)
-
-    # # Copy file to folder /Users/nic/Dropbox/Notes/kaltura/transcripts as Markdown
-    # shutil.copy2(output_file, f"/Users/nic/Dropbox/Notes/kaltura/transcripts/{uid}.md")
-    # print(f"\n{uid}.md copied to /Users/nic/Dropbox/Notes/kaltura/transcripts/")
-
-    # SRT
-    srt_writer = get_writer("srt", copy_to_path)
-    srt_output_file = f"{copy_to_path}/{uid}.srt"
-    srt_writer(result, srt_output_file)
-    print(f"\n{srt_output_file} created.")
-
-
-    return transcript
-
-
-########################################################################################################
-
-if __name__ == '__main__':
-    print()
-    # processing(file=sys.argv[1])
-    # language = 'english'
-
-    file_path = input(f"\nEnter file path to transcribe: ")
-    model_size = input(f"\nModel size (base.en, small.en, medium, medium.en, large): ")
-
-    transcribe_file(file_path,model_size)
-
-    # transcribe_file('/Users/nic/Movies/Recordings/240831-173202-test.mp4')
-    print('-------------------------------')
-    print(f"{os.path.basename(__file__)}")
-    print()
-    print()
-    print('-------------------------------')
-    run_time = round((time.time() - start_time), 1)
-    if run_time > 60:
-        print(f'{os.path.basename(__file__)} finished in {run_time/60} minutes.')
-    else:
-        print(f'{os.path.basename(__file__)} finished in {run_time}s.')
-    print()
